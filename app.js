444f684205ff01369ec4fa00d09fbc82e103723f
Paulo Tozzi <p.tozzi2016@gmail.com>
2024-08-17 18:56:04 -0300

    crud users

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const UserHandler = require('./domains/users/user.handler')
 
 const app = express()
 
-app.use('/health', (req, res) => {
+app.use(express.json())
+
+app.get('/health', (req, res) => {
     return res.json({msg: 'running...'})
 })
 
 app.get('/users', UserHandler.listAllUsers)
+
+app.get('/user/:id', UserHandler.userDetails)
+
+app.delete('/user/:id', UserHandler.removeUser)
+
+app.put('/user/:id', UserHandler.userUpdate)
+
+app.post('/user', UserHandler.createUser)
 
 module.exports = app
 
--- domains/users/user.handler.js
+++ domains/users/user.handler.js
@@ -4,3 +4,47 @@ exports.listAllUsers = async (req, res) => {
     const usersList = await User.findAll()
     return res.json(usersList)
 }
+
+exports.userDetails = async (req, res) => {
+
+    const id = req.params.id
+
+    const details = await User.findOne({where: {id}})
+
+    return res.json(details)
+}
+
+exports.createUser = async(req, res) => {
+    const {username, email, password} = req.body
+
+    const result = {
+        username: username,
+        email: email,
+        password: password
+    }
+
+    //método para gravar o usuário no banco de dados
+    const newUserAdded = await User.create(result)
+
+    //retorno do usuário gravado
+    return res.json(newUserAdded)
+}
+
+exports.removeUser = async (req, res) => {
+    const id = req.params.id
+
+    const userRemoved = await User.destroy({where: {id}})
+
+    return res.json({msg: `user ${id} removed`})
+
+}
+
+exports.userUpdate = async (req, res) => {
+    const id = req.params.id
+
+    const data = req.body
+
+    const newData = await User.update(data, {where: {id}})
+
+    return res.json({msg: `user ${id} updated`})
+}
